594ec1617bd6cc850c8cf1d87d68c2248e493bc3
Chris <cvanden8@gmail.com>
2023-04-25 23:57:06 +0100

    updating class card

--- src/components/classCard.tsx
+++ src/components/classCard.tsx
@@ -43,55 +43,63 @@ const ClassCard: React.FC<{ data: classCardProps }> = (props) => {
     _count,
   } = props.data;
   return (
-    <article className="flex flex-col gap-y-2 border border-black bg-slate-100 p-1">
-      <div className="flex items-center gap-x-2">
-        <Link href={`/instructor/${hostId}`}>
-          <Tooltip text="View Page">
-            <img
-              src={image ? image : "default"}
-              alt="test"
-              className="rounded-full"
-              height={40}
-              width={40}
-              loading="lazy"
-            />
-          </Tooltip>
-        </Link>
-        <div className="">
-          <p className="font-bold">{hostName}</p>
-          <p className="font-semibold">{title}</p>
+    <article
+      style={{
+        backgroundImage:
+          "url(https://images.unsplash.com/photo-1552196563-55cd4e45efb3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1026&q=80)",
+      }}
+      className="bg-cover outline outline-1"
+    >
+      <div className="flex flex-col gap-y-4 bg-gradient-to-b from-black/30 to-black p-2 backdrop-grayscale">
+        <div className="flex items-center gap-x-2 ">
+          <Link href={`/instructor/${hostId}`}>
+            <Tooltip text="View Page">
+              <img
+                src={image ? image : "default"}
+                alt="test"
+                className="rounded-full"
+                height={40}
+                width={40}
+                loading="lazy"
+              />
+            </Tooltip>
+          </Link>
+          <div className="">
+            <p className="font-bold">{hostName}</p>
+            <p className="font-semibold">{title}</p>
+          </div>
         </div>
-      </div>
-      <div className="flex justify-between">
-        <p>{category}</p>
-        <p
-          className={`${
-            difficultyColour[difficulty as keyof typeof difficultyColour]
-          } rounded-lg pl-1 pr-1 text-white`}
-        >
-          {difficulty}
-        </p>
-      </div>
-      <p className="truncate text-gray-600">{description}</p>
-      <p>Price: {price === 0 ? "FREE" : `£${price}`}</p>
-      <div className="flex justify-between">
-        <div className="flex items-center gap-1">
-          <p>
-            {_count.participant}/{capacity}
+        <div className="flex justify-between">
+          <p>{category}</p>
+          <p
+            className={`${
+              difficultyColour[difficulty as keyof typeof difficultyColour]
+            } rounded-lg pl-1 pr-1 text-white`}
+          >
+            {difficulty}
           </p>
-          <Image src="/personicon.png" alt="icon" height={10} width={13} />
         </div>
-        <p>{date}</p>
-      </div>
-      {/* <button onClick={() => addParticipant.mutateAsync({ classId: id })}>
+        <p className="truncate text-gray-600">{description}</p>
+        <p>Price: {price === 0 ? "FREE" : `£${price}`}</p>
+        <div className="flex justify-between">
+          <div className="flex items-center gap-1">
+            <p>
+              {_count.participant}/{capacity}
+            </p>
+            <Image src="/personicon.png" alt="icon" height={10} width={13} />
+          </div>
+          <p>{date}</p>
+        </div>
+        {/* <button onClick={() => addParticipant.mutateAsync({ classId: id })}>
         Join Class
       </button> */}
-      <Link
-        href={`/viewclasses/${id}`}
-        className="rounded-lg bg-slate-700 text-center text-white"
-      >
-        View Class
-      </Link>
+        <Link
+          href={`/viewclasses/${id}`}
+          className="rounded-lg bg-slate-800 text-center outline outline-2 hover:outline-4"
+        >
+          View Class
+        </Link>
+      </div>
     </article>
   );
 };
