902a7f96eed3c5bf77f930c608ad610106369b88
Chris <cvanden8@gmail.com>
2023-05-01 03:43:53 +0100

    temp removed stripe code

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,11 +1,11 @@
 import { router } from "../trpc";
 import { classRouter } from "./class";
-import { paymentRouter } from "./payment";
+// import { paymentRouter } from "./payment";
 import { userRouter } from "./user";
 export const appRouter = router({
   class: classRouter,
   user: userRouter,
-  payment: paymentRouter,
+  // payment: paymentRouter,
 });
 
 // export type definition of API
