22d07f57a21d71a06bd76673b35c006ed98e3da3
Chris <cvanden8@gmail.com>
2023-05-14 20:08:06 +0100

    Updated header dropdown colour

--- src/components/HeaderDropdown.tsx
+++ src/components/HeaderDropdown.tsx
@@ -22,35 +22,35 @@ const HeaderDropdown: React.FC = () => {
         <GiHamburgerMenu size={20} />
       </button>
       {isOpen && (
-        <nav className="absolute left-0 mt-2 flex w-full flex-col bg-slate-200">
+        <nav className="absolute left-0 mt-2 flex w-full flex-col bg-gray-900">
           <Link
             href="/"
-            className="w-full px-2 text-center hover:bg-slate-400 hover:underline"
+            className="w-full px-2 text-center hover:bg-slate-800 hover:underline"
           >
             Home
           </Link>
           <Link
             href="/createclass"
-            className="w-full px-2 text-center hover:bg-slate-400 hover:underline"
+            className="w-full px-2 text-center hover:bg-slate-800 hover:underline"
           >
             Create Class
           </Link>
           <Link
             href="/viewclasses"
-            className="w-full px-2 text-center hover:bg-slate-400 hover:underline"
+            className="w-full px-2 text-center hover:bg-slate-800 hover:underline"
           >
             View Classes
           </Link>
           <Link
             href="/instructor"
-            className="w-full px-2 text-center hover:bg-slate-400 hover:underline"
+            className="w-full px-2 text-center hover:bg-slate-800 hover:underline"
           >
             Instuctors
           </Link>
           {sesh && (
             <Link
               href="/profile"
-              className="w-full px-2 text-center hover:bg-slate-400 hover:underline"
+              className="w-full px-2 text-center hover:bg-slate-800 hover:underline"
             >
               Profile
             </Link>
